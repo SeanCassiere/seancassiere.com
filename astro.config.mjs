fa9f99774d7783c70edc8eeeeeea46bb2ed610b1
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2023-06-25 18:16:42 +1200

    chore: add plugins and eveything I should need

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,32 @@
-import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import image from "@astrojs/image";
+import sitemap from "@astrojs/sitemap";
+import prefetch from "@astrojs/prefetch";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: "https://seancassiere.com",
+	markdown: {
+		shikiConfig: {
+			theme: "dracula",
+			wrap: true,
+		},
+	},
+	integrations: [
+		mdx(),
+		tailwind(),
+		image({
+			serviceEntryPoint: "@astrojs/image/sharp",
+		}),
+		sitemap(),
+		prefetch(),
+	],
+	compressHTML: true,
+	vite: {
+		optimizeDeps: {
+			exclude: ["@resvg/resvg-js"],
+		},
+	},
+});
