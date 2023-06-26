e10e460122b601c434b7b9ee78a65d844e139eac
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2023-06-26 21:38:53 +1200

    feat: add rss support

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,23 @@
+import rss from "@astrojs/rss";
+import type { APIContext } from "astro";
+import { getCollection } from "astro:content";
+import { siteConfig } from "@/site-config";
+import { sortMDByDate } from "@/utils";
+
+export async function get({ site }: APIContext) {
+	const posts = await getCollection("blog", ({ data }) => data.draft !== true);
+	const sortedPosts = sortMDByDate(posts);
+
+	return rss({
+		title: `${siteConfig.author}'s blog`,
+		description: "Anything and everything I write about.",
+		site: String(site),
+		items: sortedPosts.map((post) => ({
+			title: post.data.title,
+			description: post.data.description,
+			pubDate: post.data.publishDate,
+			link: `/blog/${post.slug}`,
+		})),
+		customData: "<language>en-gb</language>",
+	});
+}
