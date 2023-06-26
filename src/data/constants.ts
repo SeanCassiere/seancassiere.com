9f09aa5d1ecdd604bdc009c69c01edf0bddeb8de
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2023-06-26 19:11:27 +1200

    refactor: the some content copy and move the excited-tech items into components

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -4,11 +4,13 @@ export const SOCIAL_LINKS = {
 	email: "me@seancassiere.com",
 };
 
-export const PROJECTS: { name: string; link: string; description: string }[] = [
+export type Project = { name: string; link: string; description: string };
+
+export const PROJECTS: Project[] = [
 	{
 		name: "ping-rents",
 		link: SOCIAL_LINKS.github + "/ping-rents",
-		description: "Basic car rental management mobile app.",
+		description: "A basic react-native car rental management mobile app.",
 	},
 	{
 		name: "seancassiere.com",
@@ -18,11 +20,42 @@ export const PROJECTS: { name: string; link: string; description: string }[] = [
 	{
 		name: "nv-reservation-cc-update",
 		link: SOCIAL_LINKS.github + "/nv-reservation-cc-update",
-		description: "Add customer credit card details into RENTALL.",
+		description:
+			"Add customer credit card details into RENTALL using this react app.",
 	},
 	{
 		name: "simple-logging-server",
 		link: SOCIAL_LINKS.github + "/simple-logging-server",
 		description: "An up and running fastify server for storing my app logs.",
+	},
+];
+
+export type ExcitedTechnology = { name: string; link: string; tagLine: string };
+
+export const EXCITED_TECHNOLOGIES: ExcitedTechnology[] = [
+	{
+		name: "Drizzle ORM",
+		link: "https://orm.drizzle.team",
+		tagLine: "Typescript ORM that let's you love SQL.",
+	},
+	{
+		name: "tRPC",
+		link: "https://trpc.io",
+		tagLine: "End-to-end typesafe APIs made easy.",
+	},
+	{
+		name: "Tanstack Query",
+		link: "https://tanstack.com/query",
+		tagLine: "Powerful asynchronous state management for React.",
+	},
+	{
+		name: "TailwindCSS",
+		link: "https://tailwindcss.com",
+		tagLine: "Rapidly build modern websites without ever leaving your HTML.",
+	},
+	{
+		name: "React",
+		link: "https://react.dev",
+		tagLine: "The library for web and native user interfaces.",
 	},
 ];
