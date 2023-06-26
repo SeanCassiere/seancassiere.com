3e18e6840ce8e32012deacccf4e4d67e9c1d519e
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2023-06-26 18:58:13 +1200

    feat: displaying my projects

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -3,3 +3,26 @@ export const SOCIAL_LINKS = {
 	twitter: "https://twitter.com/SeanCassiere",
 	email: "me@seancassiere.com",
 };
+
+export const PROJECTS: { name: string; link: string; description: string }[] = [
+	{
+		name: "ping-rents",
+		link: SOCIAL_LINKS.github + "/ping-rents",
+		description: "Basic car rental management mobile app.",
+	},
+	{
+		name: "seancassiere.com",
+		link: SOCIAL_LINKS.github + "/seancassiere.com",
+		description: "A mundane website built with Astro.",
+	},
+	{
+		name: "nv-reservation-cc-update",
+		link: SOCIAL_LINKS.github + "/nv-reservation-cc-update",
+		description: "Add customer credit card details into RENTALL.",
+	},
+	{
+		name: "simple-logging-server",
+		link: SOCIAL_LINKS.github + "/simple-logging-server",
+		description: "An up and running fastify server for storing my app logs.",
+	},
+];
