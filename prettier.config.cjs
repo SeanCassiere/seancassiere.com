0b8665b1142e3430e62fc06a1ed7af1f5b5748bf
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2023-08-24 16:05:48 +1200

    style: more prettier formatting

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -9,7 +9,6 @@ module.exports = {
 		require.resolve("prettier-plugin-astro"),
 		require.resolve("prettier-plugin-tailwindcss") /* Must come last */,
 	],
-	pluginSearchDirs: false,
 	overrides: [
 		{
 			files: "**/*astro",
