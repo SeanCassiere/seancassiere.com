b0e71e00d5212adeba58140116ef98989a1c3d37
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2024-10-14 22:27:50 +1300

    style: adjust eslint settings

--- eslint.config.js
+++ eslint.config.js
@@ -27,4 +27,24 @@ export default [
 	...tseslint.configs.recommended,
 	...eslintPluginAstro.configs["flat/recommended"],
 	eslintConfigPrettier,
+	{
+		rules: {
+			// https://typescript-eslint.io/rules/no-explicit-any/
+			"@typescript-eslint/no-explicit-any": "off",
+
+			// https://typescript-eslint.io/rules/no-unused-vars/
+			"@typescript-eslint/no-unused-vars": [
+				"error",
+				{
+					args: "all",
+					argsIgnorePattern: "^_",
+					caughtErrors: "all",
+					caughtErrorsIgnorePattern: "^_",
+					destructuredArrayIgnorePattern: "^_",
+					varsIgnorePattern: "^_",
+					ignoreRestSiblings: true,
+				},
+			],
+		},
+	},
 ];
