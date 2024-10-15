845f5f24b0efc04475a217725c1da07d01b21912
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2024-10-15 21:57:01 +1300

    feat: redesigned blog post page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -51,6 +51,127 @@ module.exports = {
 				sans: [...fontFamily.sans],
 				serif: [...fontFamily.serif],
 			},
+			typography: () => ({
+				blog: {
+					css: {
+						"--tw-prose-body": "text-foreground",
+						"--tw-prose-headings": "text-foreground",
+						"--tw-prose-links": "text-foreground",
+						"--tw-prose-bold": "text-foreground",
+						"--tw-prose-bullets": "text-foreground",
+						"--tw-prose-quotes": "text-accent",
+						"--tw-prose-code": "text-foreground",
+						"--tw-prose-hr": "0.5px dashed hsl(var(--border))",
+						"--tw-prose-th-borders": "border-border",
+					},
+				},
+				DEFAULT: {
+					css: {
+						a: {
+							textUnderlineOffset: "2px",
+						},
+						"h1 a": {
+							backgroundImage: "none",
+							textUnderlineOffset: "2px",
+							textDecorationLine: "underline",
+							textDecorationColor: "transparent",
+							transitionProperty: "all",
+							transitionTimingFunction: "cubic-bezier(0.4, 0, 0.2, 1)",
+							transitionDuration: "150ms",
+							"&:hover": {
+								backgroundImage: "none",
+								textDecorationColor: "hsl(var(--foreground))",
+							},
+						},
+						"h2 a": {
+							backgroundImage: "none",
+							textUnderlineOffset: "2px",
+							textDecorationLine: "underline",
+							textDecorationColor: "transparent",
+							transitionProperty: "all",
+							transitionTimingFunction: "cubic-bezier(0.4, 0, 0.2, 1)",
+							transitionDuration: "150ms",
+							"&:hover": {
+								backgroundImage: "none",
+								textDecorationColor: "hsl(var(--foreground))",
+							},
+						},
+						"h3 a": {
+							backgroundImage: "none",
+							textUnderlineOffset: "2px",
+							textDecorationLine: "underline",
+							textDecorationColor: "transparent",
+							transitionProperty: "all",
+							transitionTimingFunction: "cubic-bezier(0.4, 0, 0.2, 1)",
+							transitionDuration: "150ms",
+							"&:hover": {
+								backgroundImage: "none",
+								textDecorationColor: "hsl(var(--foreground))",
+							},
+						},
+						"h4 a": {
+							backgroundImage: "none",
+							textUnderlineOffset: "2px",
+							textDecorationLine: "underline",
+							textDecorationColor: "transparent",
+							transitionProperty: "all",
+							transitionTimingFunction: "cubic-bezier(0.4, 0, 0.2, 1)",
+							transitionDuration: "150ms",
+							"&:hover": {
+								backgroundImage: "none",
+								textDecorationColor: "hsl(var(--foreground))",
+							},
+						},
+						"h5 a": {
+							backgroundImage: "none",
+							textUnderlineOffset: "2px",
+							textDecorationLine: "underline",
+							textDecorationColor: "transparent",
+							transitionProperty: "all",
+							transitionTimingFunction: "cubic-bezier(0.4, 0, 0.2, 1)",
+							transitionDuration: "150ms",
+							"&:hover": {
+								backgroundImage: "none",
+								textDecorationColor: "hsl(var(--foreground))",
+							},
+						},
+						strong: {
+							fontWeight: "700",
+						},
+						code: {
+							border: "1px dotted hsl(var(--border))",
+							borderRadius: "2px",
+							"@apply font-sans": "",
+						},
+						blockquote: {
+							borderLeftWidth: "none",
+						},
+						hr: {
+							borderTopStyle: "dashed",
+						},
+						thead: {
+							borderBottomWidth: "none",
+						},
+						"thead th": {
+							fontWeight: "700",
+							borderBottom: "1px dashed hsl(var(--border))",
+						},
+						"tbody tr": {
+							borderBottomWidth: "none",
+						},
+						tfoot: {
+							borderTop: "1px dashed hsl(var(--border))",
+						},
+					},
+				},
+				sm: {
+					css: {
+						code: {
+							fontWeight: "400",
+						},
+					},
+				},
+			}),
 		},
 	},
 	plugins: [
